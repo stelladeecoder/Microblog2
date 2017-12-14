f799d4fb3b60024d36d7ec18095fc945ce9d49ce
Delores Estella Carey <stelladeecoder@gmail.com>
2017-12-13 19:37:50 -0600

    Update Main.java
    -New User shows up on the list
    -current user switches
    - last post made by user is found
    -bloggers have option to provide website
    - 4) prints all post

--- Main.java
+++ Main.java
@@ -1,106 +1,204 @@
+import java.util.ArrayList;
 import static java.lang.System.out;
 import java.util.Scanner;
-import java.util.ArrayList;
-
     public class Main
     {
+/* User [] userInfo = new User [3];
+        userInfo [0] = new User ("Chilli", "https://tinyurl.com/ybt9z6uw", "Rozonda","Thomas", "chilli@tlc.com","http://tlc.com");
+        userInfo [1] = new User ("Tboz", "https://tinyurl.com/ydynfnee", "Tionne", "Watkins", "tboz@tlc.com", "http://tlc.com");
+        userInfo [2] = new User ("LeftEye", "https://tinyurl.com/y8bpluhn", "Lisa", "Lopez", "lefteye@tlc.com", "http://tlc.com");
 
-        public static void main(String[] args)
-        {
-            Scanner keyboard = new Scanner(System.in);
-            boolean quit = false;
+        for (int l=0; l < userInfo.length; l ++)
+        { userInfo[l].printUser();
+            System.out.println("");
+        }
 
-            ArrayList<Users>allUsers= new ArrayList<Users>();
-            //*("Chilli", "https://tinyurl.com/ybt9z6uw", "Rozonda","Thomas", "chilli@tlc.com","http://tlc.com");
-            // {"Tboz", "https://tinyurl.com/ydynfnee", "Tionne", "Watkins", "tboz@tlc.com", "http://tlc.com");
-            //("LeftEye", "https://tinyurl.com/y8bpluhn", "Lisa", "Lopez", "lefteye@tlc.com", "http://tlc.com"); */
 
-            Users x=new Users( "Chilli ", "https://tinyurl.com/ybt9z6uw", "Rozonda", "Thomas", "chilli@tlc.com", "http://tlc.com");
-            Users y=new Users( "LeftEye ", "https://tinyurl.com/ydynfnee", "Tionne", "Watkins", "tboz@tlc.com", "http://tlc.com");
-            Users z=new Users( "Tboz ", "https://tinyurl.com/y8bpluhn", "Lisa", "Lopez", "lefteye@tlc.com", "http://tlc.com");
-
-            allUsers.add(x);
-            allUsers.add(y);
-            allUsers.add(z);
-
-            ArrayList<Posts>allPosts= new ArrayList<Posts>();
-             /*Post [] post = new Post [5];
+        Post [] post = new Post [5];
         post [0] = new Post (userInfo[0].getUsername(), "https://genius.com/Tlc-waterfalls-lyrics",  "Don't go chasing waterfalls");
         post [1] = new Post (userInfo[0].getUsername(), "https://genius.com/Tlc-creep-lyrics",  "The 22nd of loneliness and we've been through so many things");
         post [2] = new Post (userInfo[1].getUsername(), "https://genius.com/Tlc-diggin-on-you-lyrics",  "I was chilling with my Kool-aid when Miss Chilli came to relay");
         post [3] = new Post (userInfo[1].getUsername(), "https://genius.com/Tlc-red-light-special-lyrics",  "Take a good look at it, look at it now");
         post [4] = new Post (userInfo[2].getUsername(), "https://genius.com/Tlc-kick-your-game-lyrics", "I've been watchin' you, watchin' me" +
-                "And I know you want it" ); */
+                "And I know you want it" );
 
-            System.out.println("Please Enter An Username");
-            String answer= keyboard.nextLine();
-            do
-            {
-                System.out.println("Main Menu");
-                System.out.println("1: Create a New User");
-                System.out.println("2: Become An Existing User");
-                System.out.println("3: Create A post As The Current User");
-                System.out.println("4: Print All Posts");
-                System.out.println("5: Print All Users");
-                System.out.println("6: Exit");
-                System.out.println();
+        for (int i=0; i < post.length; i ++)
+        { post[i].printPost(); */
 
-                System.out.println("Your are currently user" + " "+ answer + " ." + "  Enter Options [1-6].");
-                int option= keyboard.nextInt();
-                System.out.println();
-                switch (option)
-                {
-                    case 1:
-                        System.out.println("Please Create a New Username");
-                        String username= keyboard.nextLine();
-                        System.out.println("Enter the url of your avatar.");
-                        String url= keyboard.nextLine();
-                        System.out.println("Enter Your First Name");
-                        String firstName= keyboard.nextLine();
-                        System.out.println("Enter Your Last Name");
-                        String lastName= keyboard.nextLine();
-                        System.out.println("Enter your email address.");
-                        String email= keyboard.nextLine();
-
-
-                        Users person = new Users("", "", "", "", "", "" );
-
-                        allUsers.add(person);
-                        break;
-                    case 2:
-                        for(int i=0; i< allUsers.size(); i++)
-                        {
-
-                            {
-                                System.out.println(allUsers.get(i).getUsername());
-                            }
-                        }
-                        System.out.println("Select An Username From Above");
-                        keyboard.nextLine();
-                        String info=keyboard.nextLine();
-                        System.out.println();
-                        break;
-
-                    case 3:
-
-                        Posts myPost= new Posts();
-                        myPost.postBody();
-                        myPost.printPost();
-                        allPosts.add(myPost);
-                        break;
-                    case 4:
-                        System.out.println(allPosts);
-                    case 5:
-                        System.out.println(allUsers);
-                        break;
-                    case 6:
-                        quit=true;
-                        break;
-                    default:
-                        System.out.println("Invalid Option Try Again.");
-                }
-                System.out.println();
-            } while (!quit);
-            System.out.println("Later Alligator!");
+        public static void printPostInfo (ArrayList<Post> p)
+        { for (int i=0; i<p.size(); i++)
+        {
+            p.get(i).printPost();
         }
+
+        }
+
+        public static void printUsername (ArrayList <User> u)
+        {for (int i = 0; i < u.size(); i++)
+        {
+            out.println((i+1) + ":" + u.get(i).getUsername());
+
+
+        }
+        }
+
+        public static Boolean findUserName(ArrayList <User> u, String username)
+        {
+
+            for (int i = 0; i < u.size(); i++)
+
+            {
+                if(username.equals(u.get(i).getUsername()))
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        public static void locateMemberPost (String memberName, ArrayList <Post> p)
+        {
+            for (int i=(p.size()-1); i>0; i--)
+
+                if (memberName.equals(p.get(i).getPostBody()))
+                {
+                    p.get(i).printPost();
+                    break;
+
+                }
+
+        }
+
+       public static void userId (ArrayList <User> u)
+        {
+            for (int i = 0; i < u.size(); i++)
+            {
+                u.get(i).printUser();
+
+            }
+        }
+
+        public static void main (String [] args)
+        {
+            Scanner keyboard = new Scanner(System.in);
+            boolean quit = false;
+            String currentUser = "Chilli ";
+            String username = "";
+            //String web = "";
+            String g = "";
+
+            User x=new User( "Chilli ", "https://tinyurl.com/ybt9z6uw", "Rozonda", "Thomas", "chilli@tlc.com");
+            User y=new User( "LeftEye ", "https://tinyurl.com/ydynfnee", "Tionne", "Watkins", "tboz@tlc.com");
+            User z=new User( "Tboz ", "https://tinyurl.com/y8bpluhn", "Lisa", "Lopez", "lefteye@tlc.com");
+
+            ArrayList <User> userInfo = new ArrayList <User> ();
+            userInfo.add(x);
+            userInfo.add(y);
+            userInfo.add(z);
+
+            Post xOne = new Post (x.getUsername(), "https://genius.com/Tlc-waterfalls-lyrics",  "Don't go chasing waterfalls");
+            Post yTwo = new Post (y.getUsername(), "https://genius.com/Tlc-creep-lyrics",  "The 22nd of loneliness and we've been through so many things");
+            Post zThree = new Post (z.getUsername(), "https://genius.com/Tlc-diggin-on-you-lyrics",  "I was chilling with my Kool-aid when Miss Chilli came to relay");
+            Post zFour = new Post (z.getUsername(), "https://genius.com/Tlc-red-light-special-lyrics",  "Take a good look at it, look at it now");
+            Post xFive = new Post (x.getUsername(), "https://genius.com/Tlc-kick-your-game-lyrics", "I've been watchin' you, watchin' me" +
+                    "And I know you want it" );
+
+
+            ArrayList <Post> postInfo = new ArrayList <Post> ();
+            postInfo.add(xOne);
+            postInfo.add(yTwo);
+            postInfo.add(zThree);
+            postInfo.add(zFour);
+            postInfo.add(xFive);
+
+                do {
+                    out.println("Welcome to Microblog Yay");
+                    out.println("Main Main");
+                    out.println("1) Create a new user");
+                    out.println("2) Become an existing user");
+                    out.println("3) Create a post as the current user");
+                    out.println("4) Print all posts");
+                    out.println("5) Print all users");
+                    out.println("6) Exit");
+                    out.println();
+                    out.println("Your are currently user" + " "+ currentUser + ":" + " Enter choice [1-6].");
+                    int option= keyboard.nextInt();
+                    keyboard.nextLine();
+                    out.println();
+
+                    switch (option) {
+                        case 1:
+                            out.println("Create Username");
+                            username = keyboard.next();
+                            out.println("Enter Avatar Url");
+                            String avatar = keyboard.next();
+                            out.println("Enter First Name");
+                            String firstName = keyboard.next();
+                            out.println("Enter Last Name.");
+                            String lastName = keyboard.next();
+                            out.println("Enter Email Address.");
+                            String email = keyboard.next();
+                            //out.println("Enter url Link.");
+                            //String url = keyboard.next();
+                            currentUser = username;
+
+                            User owner = new User(username, avatar, firstName, lastName, email);
+                            userInfo.add(owner);
+                            break;
+                        case 2:
+                            printUsername(userInfo);
+                            out.println("Select Existing User: Select A Number.");
+                            option = keyboard.nextInt();
+                            for (int i = 0; i < userInfo.size(); i++) {
+                                if (option == (i + 1))
+
+                                {
+                                    currentUser = userInfo.get(i).getUsername();
+                                    g = userInfo.get(i).getUrl();
+                                }
+                            }
+
+                            break;
+                        case 3:
+                            String answer = "";
+                            String web = "";
+                            //userPost.setName(currentUser);
+                            //userPost.setInput();
+                            out.println("What's on Your Mind?");
+                            answer = keyboard.nextLine();
+                            out.println("Link a web url to post? Enter Yes or No");
+                            String pb = keyboard.nextLine();
+                            if (pb.equals("Yes") || pb.equals("yes") || pb.equals("YES")) {
+                                out.println("Enter Website");
+                                web = keyboard.nextLine();
+
+                            } else {
+                                out.println("N/A");
+                            }
+                            Post userPost = new Post(currentUser, web, answer);
+                            //userPost.printLinkUrl();
+                            postInfo.add(userPost);
+                            break;
+                        case 4:
+                            for (int i = 0; i < postInfo.size(); i++) {
+                                postInfo.get(i).printPost();
+                            }
+                            break;
+                        case 5:
+                            for (int i = 0; i < userInfo.size(); i++) {
+                                userInfo.get(i).printUser();
+                            }
+                            break;
+                        case 6:
+                            quit = true;
+                            break;
+                        default:
+                            out.println("Invalid Option.");
+                    }
+                    out.println();
+                } while (!quit);
+                out.println("Good Bye!");
+            }
+
     }
